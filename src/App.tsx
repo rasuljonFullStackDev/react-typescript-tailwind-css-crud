ea800dca89e4f28c98d2cce3ddf04b75d022ad5e
abdurasulFulstakDev <tursunboyevabdurasuldevolop@gmail.com>
2022-10-07 11:36:24 +0500

    start crud

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
+import Add from './Add'
 import List from './List'
 
 const App = () => {
   return (
     <div>
       <List/>
+      <Add/>
     </div>
   )
 }
--- src/List.tsx
+++ src/List.tsx
@@ -1,15 +1,9 @@
 import React from 'react'
 
 const List = () => {
-    return (
-        <div>
-            <button className='bg-[red]'>as</button>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-            <h1 className='p-6 text-clip w-2 h-2 bg-[cyan] border-b border-red-500 '>salom</h1>
-        </div>
-    )
+  return (
+    <div>List</div>
+  )
 }
 
 export default List
