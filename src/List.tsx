872e49916f8374bfdd17eccc57587d3a5c2434d3
abdurasulFulstakDev <tursunboyevabdurasuldevolop@gmail.com>
2022-10-07 10:18:02 +0500

    react-typescript-tailwind-css-crud

---
+++ src/List.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const List = () => {
+    return (
+        <div>
+            <button className='bg-[red]'>as</button>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+            <h1 className='p-6 text-clip w-2 h-2 bg-[cyan] border-b border-red-500 '>salom</h1>
+        </div>
+    )
+}
+
+export default List
